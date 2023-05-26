de31b23c8e30651aa575cc58fce83c6b43edaf38
Roosham <markhamonca@codeninjas.com>
2023-05-26 19:00:20 -0400

    Bug Invaders

--- Roosham Tiku - Breakout/Assets/Score.cs
+++ Roosham Tiku - Breakout/Assets/Score.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Score : MonoBehaviour
 {
+    public TMP_Text scoreText;
+    private int score;
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(collision.gameObject);
+        if (collision.gameObject.CompareTag("Brick"))
+        {
+            Destroy(collision.gameObject);
+            score++;
+            scoreText.text = score.ToString();
+        }
     }
 }
