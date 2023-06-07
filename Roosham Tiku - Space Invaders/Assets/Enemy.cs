6eb72cf3cd615c3f7c7c1a0b2e33efdc915bbd29
Roosham <markhamonca@codeninjas.com>
2023-06-07 19:05:06 -0400

    Space Invaders End

--- Roosham Tiku - Space Invaders/Assets/Enemy.cs
+++ Roosham Tiku - Space Invaders/Assets/Enemy.cs
@@ -4,27 +4,52 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private Animator animator;
+   private Animator animator;
+   public float speed;
+   static private Vector2 direction;
 
     private void OnCollisionEnter2D(Collision2D collision)
+
     {
         animator.SetTrigger("death");
-        Destroy(gameObject);
+        Destroy(gameObject, 1f);
         Destroy(collision.gameObject);
     }
 
-   
-       
+
+
    
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        direction = Vector2.right;
+    }
+
+    private void MoveDown()
+    {
+        foreach (Enemy enemy in FindObjectsOfType(typeof(Enemy)))
+        {
+            enemy.transform.Translate(Vector2.down); 
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(direction * Time.deltaTime * speed);
+
+        if (transform.position.x > 11f)
+        {
+            direction = Vector2.left;
+            MoveDown();
+        }
+
+        if (transform.position.x < -11f)
+        {
+            direction = Vector2.right;
+            MoveDown();
+        }
+
     }
 }
