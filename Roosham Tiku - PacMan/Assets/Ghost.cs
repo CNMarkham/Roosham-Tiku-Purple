bacaade620d44566f6bf1176ed0ba30b5df3ead8
Roosham <markhamonca@codeninjas.com>
2023-08-18 19:03:42 -0400

    Pacman - 114

--- Roosham Tiku - PacMan/Assets/Ghost.cs
+++ Roosham Tiku - PacMan/Assets/Ghost.cs
@@ -4,13 +4,69 @@ using UnityEngine;
 
 public class Ghost : Movement
 {
+    public GameObject body;
+    public GameObject eyes;
+    public GameObject blue;
+    public GameObject white;
+    public bool atHome;
+    public float homeDuration;
+    private bool frightened;
+    
+    private void Awake()
+    {
+        
+    }
+
+    protected override void ChildUpdate()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+ 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         Node node = collision.GetComponent<Node>();
 
         if (node != null)
         {
             int index = Random.Range(0, node.availableDirections.Count);
+
+            if (node.availableDirections[index] == -direction)
+            {
+                index += 1;
+
+                if (index == node.availableDirections.Count)
+                {
+                    index = 0;
+                }
+            }
+
+            SetDirection(node.availableDirections[index]);
         }
+    }
+
+    private void LeaveHome()
+    {
+
+    }
+
+    public void Frighten()
+    {
+
+    }
+
+    private void Flash()
+    {
+
+    }
+
+    private void Reset()
+    {
+        
     }
 }
--- Roosham Tiku - PacMan/Assets/Node.cs
+++ Roosham Tiku - PacMan/Assets/Node.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class Node : MonoBehaviour
 {
     public LayerMask obstacleLayer;
-    public List<Vector2> availableDirection;
+    public List<Vector2> availableDirections;
     private void CheckAvailableDirection(Vector2 newDirection)
     {
         RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0f, newDirection, 1.5f, obstacleLayer);
         if (hit.collider == null) 
         {
-            availableDirection.Add(newDirection);
+            availableDirections.Add(newDirection);
         }
 
     }
     // Start is called before the first frame update
     void Start()
     {
-        availableDirection = new List<Vector2>();
+        availableDirections = new List<Vector2>();
         CheckAvailableDirection(Vector2.up);
         CheckAvailableDirection(Vector2.down);
         CheckAvailableDirection(Vector2.left);
