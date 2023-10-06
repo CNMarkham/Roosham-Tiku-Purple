1c73753812223bebc471a4f1e890df8a40b77ec6
Roosham <markhamonca@codeninjas.com>
2023-10-06 18:59:17 -0400

    Super Mario Bros - 131

--- Roosham Tiku - Super Mario Bros/Assets/Scripts/Koopa.cs
+++ Roosham Tiku - Super Mario Bros/Assets/Scripts/Koopa.cs
@@ -15,8 +15,38 @@ public class Koopa : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
-    { 
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
 
+        if (collision.transform.position.y > transform.position.y + 0.2f)
+        {
+            Debug.Log("Jump");
+            if (shelled)
+            {
+                Launch();
+            }
+            else
+            {
+                GetComponent<Animator>().SetTrigger("shell");
+                GetComponent<EnemyMovement>().speed = 0;
+                GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+                shelled = true;
+            }
+
+            Rigidbody2D playerRB = collision.gameObject.GetComponent<Rigidbody2D>();
+            playerRB.velocity = new Vector2(playerRB.velocity.x, 10);
+        }
+        else if(shelled && !shellMoving)
+        {
+            Launch();
+        }
+        else
+        {
+            collision.gameObject.GetComponent<PlayerBehaviour>().Hit();
+        }
     }
 
     private void Launch()
