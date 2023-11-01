6f706658b1e2c04353f2ba350523bce43b97fa54
Roosham <markhamonca@codeninjas.com>
2023-11-01 18:59:46 -0400

    Tetris - 29

--- Roosham Tiku - Tetris/Assets/Scripts/Tetromino.cs
+++ Roosham Tiku - Tetris/Assets/Scripts/Tetromino.cs
@@ -7,31 +7,79 @@ public class Tetromino : MonoBehaviour
     public float speed;
     private float previousTime;
     public float fallTime = 0.8f;
+    public static int height = 20;
+    public static int width = 10;
+    public Vector3 rotationPoint;
+    public bool ValidMove()
+    {
+        foreach (Transform child in transform)
+        {
+            int x = Mathf.RoundToInt(child.transform.position.x);
+            int y = Mathf.RoundToInt(child.transform.position.y);
+            if (x < 0 || y < 0 )
+            {
+                return false;
+            }
+
+            if (x >= width || y >= height)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
     // Update is called once per frame
     void Update()
     {
 
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 convertedPoint = transform.TransformPoint(rotationPoint);
+            transform.RotateAround(convertedPoint, Vector3.forward, 90);
+            //if (!ValidMove())
+            //{
+            //    transform.RotateAround(convertedPoint, Vector3.forward, -90);
+            //}
+        }
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.Translate(Vector3.left);
+            if (!ValidMove())
+            {
+                transform.Translate(Vector3.right);
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.Translate(Vector3.right);
+            if (!ValidMove())
+            {
+                transform.Translate(Vector3.left);
+            }
         }
 
         float tempTime = fallTime;
-        if (Time.time - previousTime > tempTime)
-        {
-            transform.Translate(Vector3.down * Time.deltaTime * speed);
-            previousTime = Time.time;
-        }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
             tempTime = tempTime / 10;
-            transform.Translate(Vector3.down * Time.deltaTime * speed);
         }
+
+        if (Time.time - previousTime > tempTime)
+        {
+            transform.Translate(Vector3.down);
+            previousTime = Time.time;
+            if (!ValidMove())
+            {
+                transform.Translate(Vector3.up);
+            }
+        }
+
+        
     }
 }
