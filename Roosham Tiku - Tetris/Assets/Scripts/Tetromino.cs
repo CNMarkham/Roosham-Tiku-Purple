4989a008dd184f8fb2fea62d9e3b9142bbc4c7f7
Roosham <markhamonca@codeninjas.com>
2023-11-10 18:58:20 -0500

    Purple Belt - Finale

--- Roosham Tiku - Tetris/Assets/Scripts/Tetromino.cs
+++ Roosham Tiku - Tetris/Assets/Scripts/Tetromino.cs
@@ -10,6 +10,16 @@ public class Tetromino : MonoBehaviour
     public static int height = 20;
     public static int width = 10;
     public Vector3 rotationPoint;
+    public static Transform[,] grid = new Transform[width, height];
+    public void AddToGrid()
+    {
+        foreach (Transform child in transform)
+        {
+            int x = Mathf.RoundToInt(child.transform.position.x);
+            int y = Mathf.RoundToInt(child.transform.position.y);
+            grid[x, y] = child;
+        }
+    }
     public bool ValidMove()
     {
         foreach (Transform child in transform)
@@ -25,12 +35,61 @@ public class Tetromino : MonoBehaviour
             {
                 return false;
             }
+            if (grid[x,y] != null)
+            {
+                return false;
+            }
         }
         return true;
     }
 
+    void CheckLines()
+    {
+        for(int i = height - 1; i >= 0; i--)
+        {
+            if (HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+    bool HasLine(int i)
+    {
+        for(int j=0; j < width; j++)
+        {
+            if(grid[j,i] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    void DeleteLine(int i)
+    {
+        for (int j = 0; j< width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
 
+    void RowDown(int i)
+    {
+        for (int y = i; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if(grid[x,y] != null)
+                {
+                    grid[x, y - 1] = grid[x, y];
+                    grid[x, y] = null;
+                    grid[x, y - 1].transform.position += Vector3.down;
+                }
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -76,12 +135,15 @@ public class Tetromino : MonoBehaviour
             previousTime = Time.time;
             if (!ValidMove())
             {
+               
                 transform.Translate(Vector3.up, Space.World);
+                AddToGrid();
+                CheckLines();
                 this.enabled = false;
                 FindObjectOfType<Spawner>().SpawnTetromino();
             }
         }
-
+       
         
     }
 }
